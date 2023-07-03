6825364dc2b9b8bcb34195959aa0ed4c94469429
G285032022 <228503@st.takushoku-u.ac.jp>
2023-07-03 23:55:50 +0900

    n

--- gourmet.js
+++ gourmet.js
@@ -211,10 +211,10 @@ console.log(data.results.shop[0].station_name);
 console.log(data.results.shop[0].sub_genre.name);
 //検索結果2件目
 console.log(data.results.shop[1].access);
-console.log(data.results.shop[0].address);
-console.log(data.results.shop[0].budget.name);
-console.log(data.results.shop[0].catch);
-console.log(data.results.shop[0].genre.name);
-console.log(data.results.shop[0].open);
-console.log(data.results.shop[0].station_name);
-console.log(data.results.shop[0].sub_genre.name);
+console.log(data.results.shop[1].address);
+console.log(data.results.shop[1].budget.name);
+console.log(data.results.shop[1].catch);
+console.log(data.results.shop[1].genre.name);
+console.log(data.results.shop[1].open);
+console.log(data.results.shop[1].station_name);
+console.log(data.results.shop[1].sub_genre.name);
